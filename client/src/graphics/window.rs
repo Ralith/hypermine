626594d5fcb61e0dacf97e97f9ce42d6e0918efa
danieldeankon <daniel.d.k.1994@gmail.com>
2020-02-16 19:15:37 +1030

    Added capability for keyboard rotation, increased some values.

--- client/src/graphics/window.rs
+++ client/src/graphics/window.rs
@@ -94,6 +94,8 @@ impl Window {
         let mut right = false;
         let mut up = false;
         let mut down = false;
+        let mut clockwise = false;
+        let mut anticlockwise = false;
         let mut last_frame = Instant::now();
         let mut focused = false;
         self.event_loop
@@ -105,6 +107,10 @@ impl Window {
                         + na::Vector3::y() * (up as u8 as f64 - down as u8 as f64)
                         + na::Vector3::z() * (back as u8 as f64 - forward as u8 as f64);
                     self.sim.velocity(velocity);
+                    
+                    let viewrotation = na::Vector3::new(0.0, 0.0, clockwise as u8 as f64 - anticlockwise as u8 as f64) * 5e-2;
+                    self.sim.rotate(viewrotation);
+                    
                     let this_frame = Instant::now();
                     self.sim.step(this_frame - last_frame);
                     last_frame = this_frame;
@@ -112,7 +118,7 @@ impl Window {
                 }
                 Event::DeviceEvent { event, .. } => match event {
                     DeviceEvent::MouseMotion { delta } if focused => {
-                        self.sim.rotate(na::Vector2::new(delta.0, delta.1) * 1e-3);
+                        self.sim.rotate(na::Vector3::new(delta.0, delta.1, 0.0) * 2e-3);
                     }
                     _ => {}
                 },
@@ -141,6 +147,12 @@ impl Window {
                         }
                         VirtualKeyCode::D => {
                             right = state == ElementState::Pressed;
+                        }
+                        VirtualKeyCode::Q => {
+                            anticlockwise = state == ElementState::Pressed;
+                        }
+                        VirtualKeyCode::E => {
+                            clockwise = state == ElementState::Pressed;
                         }
                         VirtualKeyCode::R => {
                             up = state == ElementState::Pressed;
