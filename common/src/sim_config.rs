8d278466bdce5fd50212bc4f4aa7ca318b25854e
Benjamin Saunders <ben.e.saunders@gmail.com>
2020-05-16 19:40:13 -0700

    Raise default view distance

--- common/src/sim_config.rs
+++ common/src/sim_config.rs
@@ -45,7 +45,7 @@ impl SimConfig {
         let meters_to_absolute = meters_to_absolute(chunk_size, voxel_size);
         SimConfig {
             rate: x.rate.unwrap_or(10),
-            view_distance: x.view_distance.unwrap_or(75.0) * meters_to_absolute,
+            view_distance: x.view_distance.unwrap_or(90.0) * meters_to_absolute,
             input_queue_size: Duration::from_millis(x.input_queue_size_ms.unwrap_or(50).into()),
             chunk_size,
             movement_speed: x.movement_speed.unwrap_or(12.0) * meters_to_absolute,
