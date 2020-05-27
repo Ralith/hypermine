027ad377612d2f23404803fddebb19ff63b5403d
DimpyRed <davidh62012@gmail.com>
2020-05-26 20:06:21 -0700

    doubled slope effect

--- client/src/worldgen.rs
+++ client/src/worldgen.rs
@@ -461,10 +461,9 @@ impl EnviroFactors {
             max_elevation: parent.max_elevation
                 + ((((3 - parent.slopeiness.rem_euclid(7)) as f64)
                     * (1.0 - (((parent.flatness as f64) - 20.0) / 10.0).tanh())
-                    / 2.0
                     + ((3 - slopeiness.rem_euclid(7)) as f64)
                         * (1.0 - (((flatness as f64) - 20.0) / 10.0).tanh())
-                        / 2.0) as i64) * rng.sample(&plus_or_minus_one) ,
+                        ) as i64) * rng.sample(&plus_or_minus_one) ,
             temperature: parent.temperature + rng.sample(&plus_or_minus_one),
             rainfall: parent.rainfall + rng.sample(&plus_or_minus_one),
             blockiness: parent.blockiness + rng.sample(&plus_or_minus_one),
