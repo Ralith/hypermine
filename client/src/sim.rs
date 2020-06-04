e4d0dff85f3e5e4a12ee22de57ce96b3439ad8d1
DimpyRed <davidh62012@gmail.com>
2020-06-04 13:01:27 -0700

    copy-pseted some stuff at the top of client's sim into node.rs

--- client/src/sim.rs
+++ client/src/sim.rs
@@ -14,13 +14,7 @@ use common::{
     Chunks, EntityId, GraphEntities, Step,
 };
 
-pub type DualGraph = Graph<Node>;
 
-pub struct Node {
-    pub state: NodeState,
-    /// We can only populate chunks which lie within a cube of populated nodes, so nodes on the edge
-    /// of the graph always have some `None` chunks.
-    pub chunks: Chunks<Chunk>,
 }
 
 /// Game state
---
+++ common/src/node.rs
@@ -0,0 +1,7 @@
+pub type DualGraph = Graph<Node>;
+
+pub struct Node {
+    pub state: NodeState,
+    /// We can only populate chunks which lie within a cube of populated nodes, so nodes on the edge
+    /// of the graph always have some `None` chunks.
+    pub chunks: Chunks<Chunk>,
