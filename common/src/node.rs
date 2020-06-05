ebe4cf6af3ce52a572734bdc79e14a648483e3e3
DimpyRed <davidh62012@gmail.com>
2020-06-05 13:55:23 -0700

    dumped stuff from chunk.rs into node.rs

--- common/src/node.rs
+++ common/src/node.rs
@@ -39,3 +39,22 @@ impl VoxelData {
         }
     }
 }
+
+
+use crate::lru_slab::SlotId;
+
+pub enum Chunk {
+    Fresh,
+    Generating,
+    Populated {
+        voxels: VoxelData,
+        surface: Option<SlotId>,
+    },
+}
+
+impl Default for Chunk {
+    fn default() -> Self {
+        Chunk::Fresh
+    }
+}
+
