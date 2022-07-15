d540d28f7ccffbde5c4d45350627e382751f02ae
Patrick Owen <patowen95@gmail.com>
2022-07-15 16:49:05 -0400

    Add disk for hyperbolic projection

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,13 +1,12 @@
-use ggez::{
-    event::{self, EventHandler},
-    graphics::{self, Color},
-    mint, Context, ContextBuilder, GameResult,
-};
+use ggez::{conf::WindowMode, event, graphics, mint, Context, ContextBuilder, GameResult};
 
 mod math;
 
 fn main() {
-    let (mut ctx, event_loop) = ContextBuilder::new("demo", "hypermine").build().unwrap();
+    let (mut ctx, event_loop) = ContextBuilder::new("demo", "hypermine")
+        .window_mode(WindowMode::default().resizable(true))
+        .build()
+        .unwrap();
     let demo = State::new(&mut ctx);
     event::run(ctx, event_loop, demo);
 }
@@ -20,22 +19,48 @@ impl State {
     }
 }
 
-impl EventHandler for State {
+impl event::EventHandler for State {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx, Color::WHITE);
+        graphics::clear(ctx, graphics::Color::new(0.2, 0.2, 0.2, 1.0));
+        let screen_coordinates = graphics::screen_coordinates(ctx);
+        let width = screen_coordinates.w;
+        let height = screen_coordinates.h;
+
+        let scale = (width.min(height) - 1.0) / 2.0;
+
+        let draw_params = graphics::DrawParam::default()
+            .offset(mint::Point2 {
+                x: -width / 2.0 / scale,
+                y: -height / 2.0 / scale,
+            })
+            .scale([scale, scale]);
+
         let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
-            mint::Point2 { x: 200.0, y: 300.0 },
-            100.0,
-            0.1,
+            mint::Point2 { x: 0.0, y: 0.0 },
+            1.0,
+            0.1 / scale,
             graphics::Color::BLACK,
         )?;
-        graphics::draw(ctx, &circle, graphics::DrawParam::default())?;
+        graphics::draw(ctx, &circle, draw_params)?;
         graphics::present(ctx)
+    }
+
+    fn resize_event(&mut self, ctx: &mut Context, width: f32, height: f32) {
+        graphics::set_screen_coordinates(
+            ctx,
+            graphics::Rect {
+                x: 0.0,
+                y: 0.0,
+                w: width,
+                h: height,
+            },
+        )
+        .unwrap();
     }
 }
