680910eb65eacaefeeb9299eb105dc5da7fb1c41
Patrick Owen <patowen95@gmail.com>
2022-12-20 00:15:07 -0500

    Add persistent velocity

--- common/src/character_controller.rs
+++ common/src/character_controller.rs
@@ -9,6 +9,7 @@ pub fn run_character_step<T>(
     cfg: &SimConfig,
     graph: &Graph<T>,
     position: &mut Position,
+    velocity: &mut na::Vector3<f32>,
     input: &CharacterInput,
     dt_seconds: f32,
 ) {
@@ -16,6 +17,7 @@ pub fn run_character_step<T>(
         cfg,
         graph,
         position,
+        velocity,
         input,
         dt_seconds,
     }
@@ -26,6 +28,7 @@ struct CharacterControllerPass<'a, T> {
     cfg: &'a SimConfig,
     graph: &'a Graph<T>,
     position: &'a mut Position,
+    velocity: &'a mut na::Vector3<f32>,
     input: &'a CharacterInput,
     dt_seconds: f32,
 }
@@ -35,11 +38,33 @@ impl<T> CharacterControllerPass<'_, T> {
         let movement = sanitize_motion_input(self.input.movement);
 
         if self.input.no_clip {
+            // If no-clip is on, the velocity field is useless, and we don't want to accidentally
+            // save velocity from when no-clip was off.
+            *self.velocity = na::Vector3::zeros();
             self.position.local *= math::translate_along(
                 &(movement * self.cfg.no_clip_movement_speed * self.dt_seconds),
             );
         } else {
-            // Placeholder: If no-clip is off, the character cannot move.
+            let old_velocity = *self.velocity;
+
+            // Update velocity
+            let current_to_target_velocity = movement * self.cfg.max_ground_speed - *self.velocity;
+            let max_delta_velocity = self.cfg.ground_acceleration * self.dt_seconds;
+            if current_to_target_velocity.norm_squared() > max_delta_velocity.powi(2) {
+                *self.velocity += current_to_target_velocity.normalize() * max_delta_velocity;
+            } else {
+                *self.velocity += current_to_target_velocity;
+            }
+
+            // Update position by using the average of the old velocity and new velocity, which has
+            // the effect of modeling a velocity that changes linearly over the timestep. This is
+            // necessary to avoid the following two issues:
+            // 1. Input lag, which would occur if only the old velocity was used
+            // 2. Movement artifacts, which would occur if only the new velocity was used. One
+            //    example of such an artifact is the player moving backwards slightly when they
+            //    stop moving after releasing a direction key.
+            self.position.local *=
+                math::translate_along(&((*self.velocity + old_velocity) * 0.5 * self.dt_seconds));
         }
 
         // Renormalize
