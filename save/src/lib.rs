e1ec6d3193e05a8f51d66f2df0060e58471a19f0
Benjamin Saunders <ben.e.saunders@gmail.com>
2023-03-06 17:22:25 -0800

    Expose transactions in save API for cheaper batched I/O

--- save/src/lib.rs
+++ save/src/lib.rs
@@ -47,18 +47,66 @@ impl Save {
         &self.meta
     }
 
-    pub fn get(&self, node_id: u128) -> Result<Option<Node>, GetError> {
+    pub fn read(&self) -> Result<ReaderGuard<'_>, redb::Error> {
         let tx = self.db.begin_read()?;
-        let nodes = tx.open_table(NODE_TABLE)?;
-        let Some(node) = nodes.get(&node_id)? else { return Ok(None); };
-        Ok(Some(Node::decode(node.value())?))
+        Ok(ReaderGuard { tx })
     }
 
-    pub fn put(&self, node_id: u128, node_state: Node) -> Result<(), redb::Error> {
-        let msg = node_state.encode_to_vec();
+    pub fn write(&mut self) -> Result<WriterGuard<'_>, redb::Error> {
         let tx = self.db.begin_write()?;
-        let mut nodes = tx.open_table(NODE_TABLE)?;
-        nodes.insert(node_id, &*msg)?;
+        Ok(WriterGuard { tx })
+    }
+}
+
+pub struct ReaderGuard<'a> {
+    tx: redb::ReadTransaction<'a>,
+}
+
+impl ReaderGuard<'_> {
+    pub fn get(&self) -> Result<Reader<'_>, redb::Error> {
+        let table = self.tx.open_table(NODE_TABLE)?;
+        Ok(Reader { table })
+    }
+}
+
+pub struct Reader<'a> {
+    table: redb::ReadOnlyTable<'a, u128, &'static [u8]>,
+}
+
+impl Reader<'_> {
+    pub fn get(&mut self, node_id: u128) -> Result<Option<Node>, GetError> {
+        let Some(node) = self.table.get(&node_id)? else { return Ok(None); };
+        Ok(Some(Node::decode(node.value())?))
+    }
+}
+
+pub struct WriterGuard<'a> {
+    tx: redb::WriteTransaction<'a>,
+}
+
+impl<'a> WriterGuard<'a> {
+    pub fn get(&mut self) -> Result<Writer<'a, '_>, redb::Error> {
+        Ok(Writer {
+            table: self.tx.open_table(NODE_TABLE)?,
+            plain: Vec::new(),
+        })
+    }
+
+    pub fn commit(self) -> Result<(), redb::Error> {
+        self.tx.commit()
+    }
+}
+
+pub struct Writer<'save, 'guard> {
+    table: redb::Table<'save, 'guard, u128, &'static [u8]>,
+    plain: Vec<u8>,
+}
+
+impl Writer<'_, '_> {
+    pub fn put(&mut self, node_id: u128, state: &Node) -> Result<(), redb::Error> {
+        self.plain.clear();
+        state.encode(&mut self.plain).unwrap();
+        self.table.insert(node_id, &*self.plain)?;
         Ok(())
     }
 }
