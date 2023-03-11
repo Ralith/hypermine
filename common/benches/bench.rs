20000587b44f28d3532883b977e965e2af5e3634
Benjamin Saunders <ben.e.saunders@gmail.com>
2023-03-11 12:25:07 -0800

    Worldgen benchmarks

--- common/benches/bench.rs
+++ common/benches/bench.rs
@@ -1,8 +1,13 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 use common::{
-    dodeca::Side,
+    dodeca::{Side, Vertex},
     graph::{Graph, NodeId},
+    node::Chunk,
+    node::{populate_fresh_nodes, ChunkId, DualGraph},
+    proto::Position,
+    traversal::ensure_nearby,
+    worldgen::ChunkParams,
 };
 
 fn build_graph(c: &mut Criterion) {
@@ -15,6 +20,42 @@ fn build_graph(c: &mut Criterion) {
                 n = graph.ensure_neighbor(n, Side::J);
             }
             assert_eq!(graph.len(), 1001);
+        })
+    });
+
+    c.bench_function("nodegen 1000", |b| {
+        b.iter(|| {
+            let mut graph = DualGraph::new();
+            let mut n = NodeId::ROOT;
+            for _ in 0..500 {
+                n = graph.ensure_neighbor(n, Side::A);
+                n = graph.ensure_neighbor(n, Side::J);
+            }
+            assert_eq!(graph.len(), 1001);
+            populate_fresh_nodes(&mut graph);
+        })
+    });
+
+    c.bench_function("worldgen", |b| {
+        b.iter(|| {
+            let mut graph = DualGraph::new();
+            ensure_nearby(&mut graph, &Position::origin(), 3.0);
+            let fresh = graph.fresh().to_vec();
+            populate_fresh_nodes(&mut graph);
+            let mut n = 0;
+            for node in fresh {
+                for vertex in Vertex::iter() {
+                    let chunk = ChunkId::new(node, vertex);
+                    if let Some(params) = ChunkParams::new(12, &graph, chunk) {
+                        graph[chunk] = Chunk::Populated {
+                            voxels: params.generate_voxels(),
+                            surface: None,
+                        };
+                        n += 1;
+                    }
+                }
+            }
+            assert_eq!(n, 640);
         })
     });
 }
